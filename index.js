559be4d2c62d413993f9a866209a1a7698460785
akhilesh.nevatia@gmail.com <akhilesh.nevatia@gmail.com>
2023-03-12 19:51:40 -0500

    Adding modifies Drake and Ariana pages to the project

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ fetch('drakeInventory.json')
       // Create HTML elements to display product info
       const productName = document.createElement('h3');
       productName.innerText = product.name;
+      productName.classList.add('product-name');
 
       const productDescription = document.createElement('p');
       productDescription.innerText = product.description;
+      productDescription.classList.add('product-description');
 
       const productImage = document.createElement('img');
       productImage.src = product.image;
       productImage.alt = product.name;
+      productImage.classList.add('product-image');
 
       const productPrice = document.createElement('p');
       productPrice.innerText = `$${product.price}`;
+      productPrice.classList.add('product-price');
 
       // Append product elements to a container element
       const productContainer = document.createElement('div');
+      productContainer.classList.add('product-container');
       productContainer.appendChild(productName);
       productContainer.appendChild(productDescription);
       productContainer.appendChild(productImage);
